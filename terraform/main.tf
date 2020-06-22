540f7df331c40f10e0be353563e93adea80ff18d
Georgi Berchev <georgiman24@abv.bg>
2020-06-22 17:44:33 +0300

    trigger pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ variable dummy {
 
 resource null_resource echo_dummy {
   provisioner local-exec {
-    command = "echo Variableddddd is ${var.dummy}"
+    command = "echo Variabledddddd is ${var.dummy}"
   }
 
   triggers = {
